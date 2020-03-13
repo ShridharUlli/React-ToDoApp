8585e14999e5c295390ce1fe0294443f2e1da883
ShridharUlli <shridharulli@gmail.com>
2020-03-13 17:28:42 +0530

    Updates

---
+++ precache-manifest.6eebd5b65bb7ba712e89d3c8275aad98.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fa3c64761f6051bc61f93b598188bae",
+    "url": "/React-ToDoApp/index.html"
+  },
+  {
+    "revision": "fe1a20bf27f6dd35af4d",
+    "url": "/React-ToDoApp/static/css/main.77ddfef1.chunk.css"
+  },
+  {
+    "revision": "8544e9779bd24ca88af7",
+    "url": "/React-ToDoApp/static/js/2.112e9276.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-ToDoApp/static/js/2.112e9276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe1a20bf27f6dd35af4d",
+    "url": "/React-ToDoApp/static/js/main.79503446.chunk.js"
+  },
+  {
+    "revision": "6af783f4d9298642da65",
+    "url": "/React-ToDoApp/static/js/runtime-main.76720cf8.js"
+  },
+  {
+    "revision": "af52c81cc199d72d991eaee1d694c890",
+    "url": "/React-ToDoApp/static/media/Biennale-Bold.af52c81c.ttf"
+  },
+  {
+    "revision": "e7146943a43ebdd0f09b91ed165c359d",
+    "url": "/React-ToDoApp/static/media/Biennale-Light.e7146943.ttf"
+  },
+  {
+    "revision": "147f2083562c729c0ed9edb52a6038d2",
+    "url": "/React-ToDoApp/static/media/Biennale-Regular.147f2083.ttf"
+  }
+]);
